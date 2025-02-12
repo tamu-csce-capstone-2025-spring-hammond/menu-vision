ea354ee3fc32cd808e888f16e2b13f1138c2525f
Anil Parthasarathi <anilp@RX-78-Infinite-7.local>
2025-02-12 11:56:32 -0600

    Adding Reality Kit starter code

---
+++ MenuVisionTests/MenuVisionTests.swift
@@ -0,0 +1,15 @@
+//
+//  MenuVisionTests.swift
+//  MenuVisionTests
+//
+
+import Testing
+@testable import MenuVision
+
+struct MenuVisionTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
