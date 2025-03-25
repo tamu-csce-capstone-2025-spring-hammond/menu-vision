2f5e84655f9943ad77b14bceb937d5bbdc4bb5ef
Anil-Parthasarathi <61090960+Anil-Parthasarathi@users.noreply.github.com>
2025-03-25 17:38:55 -0500

    Merging ARenhancements

--- frontend/MenuVisionUI/TabViewPractice/FirstTabView.swift
+++ frontend/MenuVisionUI/TabViewPractice/FirstTabView.swift
@@ -1,216 +1,59 @@
-//
-//  ScanView.swift
-//  MenuVision
-//
-//  Created by Kim Chen on 3/2/25.
-//
-
-
-//import SwiftUI
-//import RealityKit
-//
-//extension ObjectCaptureSession.CaptureState {
-//
-//    var label: String {
-//        switch self {
-//        case .initializing:
-//            "initializing"
-//        case .ready:
-//            "ready"
-//        case .detecting:
-//            "detecting"
-//        case .capturing:
-//            "capturing"
-//        case .finishing:
-//            "finishing"
-//        case .completed:
-//            "completed"
-//        case .failed(let error):
-//            "failed: \(String(describing: error))"
-//        @unknown default:
-//            fatalError("unknown default: \(self)")
-//        }
-//    }
-//}
-//
-//extension ScanView {
-//
-//    // creates directory to hold scans (available only in app's sandboxed documents directory)
-//    func createNewScanDirectory() -> URL? {
-//        guard let capturesFolder = getRootScansFolder() else { return nil }
-//
-//        let formatter = ISO8601DateFormatter()
-//        let timestamp = formatter.string(from: Date())
-//        let newCaptureDirectory = capturesFolder.appendingPathComponent(timestamp,
-//                                                                        isDirectory: true)
-//        print("Start creating capture path: \(newCaptureDirectory)")
-//        let capturePath = newCaptureDirectory.path
-//        do {
-//            try FileManager.default.createDirectory(atPath: capturePath,
-//                                                    withIntermediateDirectories: true)
-//        } catch {
-//            print("Failed to create capture path: \(capturePath) with error: \(String(describing: error))")
-//        }
-//
-//        var isDirectory: ObjCBool = false
-//        let exists = FileManager.default.fileExists(atPath: capturePath,
-//                                                    isDirectory: &isDirectory)
-//        guard exists, isDirectory.boolValue else { return nil }
-//        print("New capture path was created")
-//        return newCaptureDirectory
-//    }
-//
-//    private func getRootScansFolder() -> URL? {
-//        guard let documentFolder = try? FileManager.default.url(for: .documentDirectory,
-//                                                                in: .userDomainMask,
-//                                                                appropriateFor: nil,
-//                                                                create: false)
-//        else { return nil }
-//        return documentFolder.appendingPathComponent("Scans/", isDirectory: true)
-//    }
-//}
-//
-//// this button handles both detection and capturing, depending on the session's current state
-//@MainActor
-//struct CreateButton: View {
-//    let session: ObjectCaptureSession
-//
-//    var body: some View {
-//        Button(action: {
-//            performAction()
-//        }, label: {
-//            Text(label)
-//            .foregroundStyle(.white)
-//            .padding()
-//            .background(.tint)
-//            .clipShape(Capsule())
-//        })
-//    }
-//
-//    private var label: LocalizedStringKey {
-//        if session.state == .ready {
-//            return "Start detecting"
-//        } else if session.state == .detecting {
-//            return "Start capturing"
-//        } else {
-//            return "Undefined"
-//        }
-//    }
-//
-//    private func performAction() {
-//        if session.state == .ready {
-//            let isDetecting = session.startDetecting()
-//            print(isDetecting ? "Start detecting" : "Not detecting")
-//        } else if session.state == .detecting {
-//            session.startCapturing()
-//        } else {
-//            print("Undefined")
-//        }
-//    }
-//}
-//
-//struct ScanView: View {
-//    @State private var session: ObjectCaptureSession?
-//    @State private var imageFolderPath: URL?
-//
-//    var body: some View {
-//        ZStack(alignment: .bottom) {
-//            if let session {
-//                ObjectCaptureView(session: session)
-//                
-//                VStack(spacing: 16) {
-//                  
-//                    if session.state == .ready || session.state == .detecting {
-//                        CreateButton(session: session)
-//                    }
-//                    
-//                    Text(session.state.label)
-//                        .bold()
-//                        .foregroundStyle(.yellow)
-//                        .padding(.bottom)
-//                }
-//            }
-//        }
-//        // this allows the directory and session to be created/started in the background
-//        .task {
-//            guard let directory = createNewScanDirectory() else { return }
-//            if (ObjectCaptureSession.isSupported == true){
-//                session = ObjectCaptureSession()
-//                imageFolderPath = directory.appending(path: "Images/")
-//                guard let imageFolderPath else { return }
-//                session?.start(imagesDirectory: imageFolderPath)
-//            }
-//        }
-//        // upon finishing the scan, we will change our session state to 'finished'
-//        .onChange(of: session?.userCompletedScanPass) { _, newValue in
-//            if let newValue,
-//               newValue {
-//                session?.finish()
-//            }
-//        }
-//    }
-//}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//////
-//////  FirstTabView.swift
-//////  MenuVisionUI
-//////
-//////  Created by Sam Zhou on 3/1/25.
-////
 import SwiftUI
-
+import RealityKit
+import ARKit
 
 struct FirstTabView: View {
+    
+    @StateObject private var viewManager = ARViewManager();
+    @State private var tappedIcon: Int = 0;
+    
     var body: some View {
-        VStack {
-            Text("Home Screen")
-                .font(.largeTitle)
-                .padding()
-            Image(systemName: "house.fill")
-                .resizable()
-                .scaledToFit()
-                .frame(width: 100, height: 100)
+        ZStack {
+            // ARViewContainer is the AR view from your AR files.
+            ARViewContainer(viewManager: viewManager)
+                .edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Spacer()
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 0) {
+                        ForEach(viewManager.getModelMap().sorted(by: { $0.key < $1.key }), id: \.key) { id, value in
+                            Button(
+                                action: {
+                                    print("Selected: \(id)")
+                                    viewManager.changeModel(index: id);
+                                    tappedIcon = id;
+                                })
+                            {
+                                Image(value)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(height: 70)
+                                    .frame(width: 70)
+                                    .background(tappedIcon == id ? Color(hex: "ff3f00").opacity(0.5) : Color(hex: "73edad").opacity(0.4))
+                                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                                    .scrollTransition(.interactive, axis: .horizontal) { effect, phase in
+                                        effect
+                                            .scaleEffect(phase.isIdentity ? 1.0 : 0.95)
+                                    }
+
+                            }
+                            .foregroundColor(Color.init(hex: "73edad"))
+                            .padding(5)
+                        }
+                    }
+                    .frame(height: 70)
+                    .background(Color.clear.allowsHitTesting(false))
+                    .padding()
+                }
+
+            }
         }
     }
+}
+
+#Preview {
+    FirstTabView()
 }
 
 
