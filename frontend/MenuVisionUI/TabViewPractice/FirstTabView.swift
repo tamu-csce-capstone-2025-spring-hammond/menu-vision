31db2e57f40ec5a539c79269088ce2659162ee06
kimcchen <kimberlykchen@gmail.com>
2025-03-25 23:11:25 -0500

    photogrammetry

--- frontend/MenuVisionUI/TabViewPractice/FirstTabView.swift
+++ frontend/MenuVisionUI/TabViewPractice/FirstTabView.swift
@@ -33,42 +33,73 @@ extension ObjectCaptureSession.CaptureState {
     }
 }
 
-extension ScanView {
-
-    // creates directory to hold scans (available only in app's sandboxed documents directory)
-    func createNewScanDirectory() -> URL? {
-        guard let capturesFolder = getRootScansFolder() else { return nil }
-
-        let formatter = ISO8601DateFormatter()
-        let timestamp = formatter.string(from: Date())
-        let newCaptureDirectory = capturesFolder.appendingPathComponent(timestamp,
-                                                                        isDirectory: true)
-        print("Start creating capture path: \(newCaptureDirectory)")
-        let capturePath = newCaptureDirectory.path
-        do {
-            try FileManager.default.createDirectory(atPath: capturePath,
-                                                    withIntermediateDirectories: true)
-        } catch {
-            print("Failed to create capture path: \(capturePath) with error: \(String(describing: error))")
-        }
-
-        var isDirectory: ObjCBool = false
-        let exists = FileManager.default.fileExists(atPath: capturePath,
-                                                    isDirectory: &isDirectory)
-        guard exists, isDirectory.boolValue else { return nil }
-        print("New capture path was created")
-        return newCaptureDirectory
-    }
-
-    private func getRootScansFolder() -> URL? {
-        guard let documentFolder = try? FileManager.default.url(for: .documentDirectory,
-                                                                in: .userDomainMask,
-                                                                appropriateFor: nil,
-                                                                create: false)
-        else { return nil }
-        return documentFolder.appendingPathComponent("Scans/", isDirectory: true)
-    }
-}
+//extension ScanView {
+//
+//    // creates directory to hold scans (available only in app's sandboxed documents directory)
+//    func createNewScanDirectory() -> URL? {
+//        guard let capturesFolder = getRootScansFolder() else { return nil }
+//
+//        let formatter = ISO8601DateFormatter()
+//        let timestamp = formatter.string(from: Date())
+//        let newCaptureDirectory = capturesFolder.appendingPathComponent(timestamp,
+//                                                                        isDirectory: true)
+//        print("Start creating capture path: \(newCaptureDirectory)")
+//        let capturePath = newCaptureDirectory.path
+//        do {
+//            try FileManager.default.createDirectory(atPath: capturePath,
+//                                                    withIntermediateDirectories: true)
+//        } catch {
+//            print("Failed to create capture path: \(capturePath) with error: \(String(describing: error))")
+//        }
+//
+//        var isDirectory: ObjCBool = false
+//        let exists = FileManager.default.fileExists(atPath: capturePath,
+//                                                    isDirectory: &isDirectory)
+//        guard exists, isDirectory.boolValue else { return nil }
+//        print("New capture path was created")
+//        return newCaptureDirectory
+//    }
+//
+//    private func getRootScansFolder() -> URL? {
+//        guard let documentFolder = try? FileManager.default.url(for: .documentDirectory,
+//                                                                in: .userDomainMask,
+//                                                                appropriateFor: nil,
+//                                                                create: false)
+//        else { return nil }
+//        return documentFolder.appendingPathComponent("Scans/", isDirectory: true)
+//    }
+//
+//    private func startReconstruction() async {
+////        print("At Reconstruction")
+//        guard let imageFolderPath,
+//              let modelPath else { return }
+//        isProgressing = true
+//        print("At Reconstruction and \(isProgressing)")
+//        do {
+//            photogrammetrySession = try PhotogrammetrySession(input: imageFolderPath)
+//            guard let photogrammetrySession else { return }
+//            try photogrammetrySession.process(requests: [.modelFile(url: modelPath)])
+//            for try await output in photogrammetrySession.outputs {
+//                switch output {
+//                case .requestError, .processingCancelled:
+//                    isProgressing = false
+//                    self.photogrammetrySession = nil
+//                    // TODO: Restart ObjectCapture
+//                case .processingComplete:
+//                    isProgressing = false
+//                    self.photogrammetrySession = nil
+//                    print("Done! \(modelPath)")
+//                    quickLookIsPresented = true
+//                default:
+//                    break
+//                }
+//            }
+//
+//        } catch {
+//            print("error", error)
+//        }
+//    }
+//}
 
 // this button handles both detection and capturing, depending on the session's current state
 @MainActor
@@ -109,44 +140,236 @@ struct CreateButton: View {
     }
 }
 
+//struct ScanView: View {
+//    @State private var session: ObjectCaptureSession?
+//    @State private var imageFolderPath: URL?
+//    @State private var photogrammetrySession: PhotogrammetrySession?
+//    @State private var modelFolderPath: URL?
+//    @State private var isProgressing = false
+//
+//    @State private var quickLookIsPresented = false
+//
+//    var modelPath: URL? {
+//        return modelFolderPath?.appending(path: "model.usdz")
+//    }
+//
+//    var body: some View {
+//        ZStack(alignment: .bottom) {
+//            if let session {
+//                ObjectCaptureView(session: session)
+//
+//                VStack(spacing: 16) {
+//
+//                    if session.state == .ready || session.state == .detecting {
+//                        CreateButton(session: session)
+//                    }
+//
+//                    Text(session.state.label)
+//                        .bold()
+//                        .foregroundStyle(.yellow)
+//                        .padding(.bottom)
+//                }
+//            }
+//            if isProgressing {
+//                Color.black.opacity(0.4)
+//                .overlay {
+//                    VStack {
+//                        ProgressView()
+//                    }
+//                }
+//            }
+//        }
+//        // this allows the directory and session to be created/started in the background
+//        .task {
+//            guard let directory = createNewScanDirectory() else { return }
+//            if (ObjectCaptureSession.isSupported == true){
+//                session = ObjectCaptureSession()
+//                modelFolderPath = directory.appending(path: "Models/")
+//                imageFolderPath = directory.appending(path: "Images/")
+//                guard let imageFolderPath else { return }
+//                session?.start(imagesDirectory: imageFolderPath)
+//            }
+//        }
+//        // upon finishing the scan, we will change our session state to 'finished'
+//        .onChange(of: session?.userCompletedScanPass) { _, newValue in
+//            if let newValue,
+//               newValue {
+//                session?.finish()
+//            }
+//        }
+//        .onChange(of: session?.state) { _, newValue in
+//            if newValue == .completed {
+//                session = nil
+//
+//                Task {
+//                    print("Starting Reconstruction")
+//                    await startReconstruction()
+//                }
+//            }
+//        }
+//        .sheet(isPresented: $quickLookIsPresented) {
+//
+//            if let modelPath {
+//                ARQuickLookView(modelFile: modelPath) {
+//                    guard let directory = createNewScanDirectory()
+//                    else { return }
+//                    quickLookIsPresented = false
+//                    // TODO: Restart ObjectCapture
+//                }
+//            }
+//        }
+//    }
+//}
+
 struct ScanView: View {
+    
     @State private var session: ObjectCaptureSession?
     @State private var imageFolderPath: URL?
-
+    @State private var photogrammetrySession: PhotogrammetrySession?
+    @State private var modelFolderPath: URL?
+    @State private var isProgressing = false
+    @State private var quickLookIsPresented = false
+    
+    var modelPath: URL? {
+        return modelFolderPath?.appending(path: "model.usdz")
+    }
+    
     var body: some View {
+        
         ZStack(alignment: .bottom) {
             if let session {
                 ObjectCaptureView(session: session)
                 
                 VStack(spacing: 16) {
-                  
+                    
                     if session.state == .ready || session.state == .detecting {
+                        // Detect and Capture
                         CreateButton(session: session)
                     }
                     
-                    Text(session.state.label)
-                        .bold()
-                        .foregroundStyle(.yellow)
-                        .padding(.bottom)
+                    HStack {
+                        Text(session.state.label)
+                            .bold()
+                            .foregroundStyle(.yellow)
+                            .padding(.bottom)
+                    }
+                    
                 }
             }
-        }
-        // this allows the directory and session to be created/started in the background
-        .task {
-            guard let directory = createNewScanDirectory() else { return }
-            if (ObjectCaptureSession.isSupported == true){
-                session = ObjectCaptureSession()
-                imageFolderPath = directory.appending(path: "Images/")
-                guard let imageFolderPath else { return }
-                session?.start(imagesDirectory: imageFolderPath)
+            
+            if isProgressing {
+                Color.black.opacity(0.4)
+                    .overlay {
+                        VStack {
+                            ProgressView()
+                        }
+                    }
             }
+            
         }
-        // upon finishing the scan, we will change our session state to 'finished'
+        .task {
+            guard let directory = createNewScanDirectory()
+            else { return }
+            session = ObjectCaptureSession()
+            
+            modelFolderPath = directory.appending(path: "Models/")
+            imageFolderPath = directory.appending(path: "Images/")
+            guard let imageFolderPath else { return }
+            session?.start(imagesDirectory: imageFolderPath)
+        }
         .onChange(of: session?.userCompletedScanPass) { _, newValue in
             if let newValue,
                newValue {
+                // This time, I've completed one scan pass.
+                // However, Apple recommends that the scan pass should be done three times.
                 session?.finish()
             }
+        }
+        .onChange(of: session?.state) { _, newValue in
+            if newValue == .completed {
+                session = nil
+                
+                Task {
+                    await startReconstruction()
+                }
+            }
+        }
+        .sheet(isPresented: $quickLookIsPresented) {
+            
+            if let modelPath {
+                ARQuickLookView(modelFile: modelPath) {
+                    guard let directory = createNewScanDirectory()
+                    else { return }
+                    quickLookIsPresented = false
+                    // TODO: Restart ObjectCapture
+                }
+            }
+        }
+    }
+}
+
+extension ScanView {
+    
+    func createNewScanDirectory() -> URL? {
+        guard let capturesFolder = getRootScansFolder()
+        else { return nil }
+        
+        let formatter = ISO8601DateFormatter()
+        let timestamp = formatter.string(from: Date())
+        let newCaptureDirectory = capturesFolder.appendingPathComponent(timestamp,
+                                                                        isDirectory: true)
+        print("Start creating capture path: \(newCaptureDirectory)")
+        let capturePath = newCaptureDirectory.path
+        do {
+            try FileManager.default.createDirectory(atPath: capturePath,
+                                                    withIntermediateDirectories: true)
+        } catch {
+            print("Failed to create capture path: \(capturePath) with error: \(String(describing: error))")
+        }
+        
+        var isDirectory: ObjCBool = false
+        let exists = FileManager.default.fileExists(atPath: capturePath,
+                                                    isDirectory: &isDirectory)
+        guard exists, isDirectory.boolValue
+        else { return nil }
+        print("New capture path was created")
+        return newCaptureDirectory
+    }
+    
+    private func getRootScansFolder() -> URL? {
+        guard let documentFolder = try? FileManager.default.url(for: .documentDirectory,
+                                                                in: .userDomainMask,
+                                                                appropriateFor: nil,
+                                                                create: false)
+        else { return nil }
+        return documentFolder.appendingPathComponent("Scans/", isDirectory: true)
+    }
+    
+    private func startReconstruction() async {
+        guard let imageFolderPath,
+              let modelPath else { return }
+        isProgressing = true
+        do {
+            photogrammetrySession = try PhotogrammetrySession(input: imageFolderPath)
+            guard let photogrammetrySession else { return }
+            try photogrammetrySession.process(requests: [.modelFile(url: modelPath)])
+            for try await output in photogrammetrySession.outputs {
+                switch output {
+                case .requestError, .processingCancelled:
+                    isProgressing = false
+                    self.photogrammetrySession = nil
+                    // TODO: Restart ObjectCapture
+                case .processingComplete:
+                    isProgressing = false
+                    self.photogrammetrySession = nil
+                    quickLookIsPresented = true
+                default:
+                    break
+                }
+            }
+            
+        } catch {
+            print("error", error)
         }
     }
 }
@@ -212,5 +435,6 @@ struct ScanView: View {
 //        }
 //    }
 //}
+
 
 
