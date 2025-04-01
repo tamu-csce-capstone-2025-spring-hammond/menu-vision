0c2f934db3e30969eb9da967c0a779159c333b7b
Anil-Parthasarathi <61090960+Anil-Parthasarathi@users.noreply.github.com>
2025-04-01 16:04:03 -0500

    Style updates

--- frontend/MenuVisionUI/HomeView.swift
+++ frontend/MenuVisionUI/HomeView.swift
@@ -1,16 +1,16 @@
 import SwiftUI
 
 struct HomeView: View {
-    @State private var selection = 2
+    @State private var selection = 1
 
     var body: some View {
-        TabView {
+        TabView(selection: $selection) {
 
             ScanView()
                 .tabItem {
                     Label("Model Scan", systemImage: "camera")
                 }
-                .tag(2)
+                .tag(0)
             
             NavigationStack {
                 MenuScannerView()
@@ -18,15 +18,37 @@ struct HomeView: View {
             .tabItem {
                 Label("Home", systemImage: "house")
             }
-            .tag(4)
+            .tag(1)
 
             FirstTabView()
                 .tabItem {
                     Label("Profile", systemImage: "person")
                 }
-                .tag(3)
+                .tag(2)
 
         }
+        .accentColor(.orange300)
+        /*.gesture(
+            DragGesture()
+                .onEnded { gesture in
+                    let horizontalDrag = gesture.translation.width;
+                    let verticalDrag = gesture.translation.height;
+                    
+                    if abs(horizontalDrag) > abs(verticalDrag) {
+                        
+                        if (horizontalDrag > 0){
+                            //right swipe occured
+                            
+                            selection = (selection + selection - 1) % 3;
+                            
+                        }
+                        else{
+                            selection = (selection + 1) % 3;
+                        }
+                        
+                    }
+                }
+        )*/
     }
 }
 
