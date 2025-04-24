fe949326d1c28382687deeb8c3ebafa7c370fc4d
Albert Yin <albertapplea@gmail.com>
2025-04-24 16:26:24 -0500

    Added Progress Bar for scans

---
+++ frontend/MenuVisionUI/TabViewPractice/TabViewComponents/ProgressBar.swift
@@ -0,0 +1,57 @@
+//
+//  ProgressBar.swift
+//  MenuVision
+//
+//  Created by Albert Yin on 4/24/25.
+//
+
+import SwiftUI
+import WebKit
+
+struct ProgressBar: View {
+    var body: some View {
+        VStack(spacing: 1) {
+            GIFView(gifName: "progressLoader")
+                .frame(width: 300, height: 300)
+                .accessibilityLabel("Loading")
+
+            Text("Cooking up your meal...")
+                .font(.headline)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
+struct GIFView: UIViewRepresentable {
+    let gifName: String
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.isUserInteractionEnabled = false
+        webView.scrollView.isScrollEnabled = false
+        webView.backgroundColor = .clear
+        webView.isOpaque = false
+
+        if let path = Bundle.main.path(forResource: gifName, ofType: "gif") {
+            let data = try? Data(contentsOf: URL(fileURLWithPath: path))
+            webView.load(data!, mimeType: "image/gif", characterEncodingName: "UTF-8", baseURL: URL(fileURLWithPath: path))
+        }
+
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        // No update needed
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ZStack {
+            Color.white
+            ProgressBar()
+        }
+        .frame(width: 150, height: 150)
+        .previewLayout(.sizeThatFits)
+    }
+}
