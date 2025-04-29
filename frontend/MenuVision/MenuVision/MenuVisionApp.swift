1ef99c8bb81d13d99fcc5853872d7dc92fdf1126
Ethan Nguyen <nguyeeth@tamu.edu>
2025-04-28 20:52:12 -0500

    Recreate clean MenuVision project

---
+++ frontend/MenuVision/MenuVision/MenuVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuVisionApp.swift
+//  MenuVision
+//
+//  Created by Ethan Nguyen on 4/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
